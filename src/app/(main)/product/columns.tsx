acf88cf57e1e5f16667a140cb85bf0f0bf16760c
LangRaZ <rangga.sekadau@gmail.com>
2025-02-25 16:41:52 +0700

    Tabel Product

---
+++ src/app/(main)/product/columns.tsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "@/components/ui/button"
+import { Pencil, Trash } from "lucide-react"
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string
+  product: string
+  category : string
+  price : string
+  stock : number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+  action : boolean
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "product",
+    header: "Product",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+  },
+  {
+    accessorKey: "action",
+    header: "Action",
+    cell: ({ row }) => {
+        const product = row.original;
+
+        return (
+          <div className="flex gap-2">
+            <Button 
+              variant="outline"
+              size="sm"
+              onClick={() => console.log("Edit", product.id)}
+              className="flex items-center gap-1"
+            >
+              <Pencil className="w-4 h-4" />
+            </Button>
+            <Button 
+              variant="destructive"
+              size="sm"
+              onClick={() => console.log("Delete", product.id)}
+              className="flex items-center gap-1"
+            >
+              <Trash className="w-4 h-4" />
+            </Button>
+          </div>
+        );
+      },
+  },
+   
+]
--- src/app/(main)/product/page.tsx
+++ src/app/(main)/product/page.tsx
@@ -1,9 +1,38 @@
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+import {SquarePlus} from "lucide-react"
 
-export default function Product
-() {
+import { Button } from "@/components/ui/button"
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      product: "Nasi Hainam",
+      category: "Hainam",
+      status: "pending",
+      email: "m@example.com",
+      price: "Rp.22000",
+      stock: 0 ,
+      action : true,
+    },
+    // ...
+  ]
+}
+
+  
+export default async function DemoPage() {
+  const data = await getData()
+
   return (
-    <div>Product
-
-    </div>
+    <>
+    <div className="container mx-auto py-5" ><Button>
+      <SquarePlus />Add Product
+    </Button></div>
+    
+    <div className="container mx-auto">
+    <DataTable columns={columns} data={data} />
+  </div></>
+    
   )
 }
